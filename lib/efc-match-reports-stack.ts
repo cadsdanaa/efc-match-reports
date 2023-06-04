195c84ab931b9b6d3c40021b4ca88139d301237a
Alex Dana <cadsdanaa@gmail.com>
2023-06-04 14:54:53 -0500

    Initial commit for efc-match-reports. Getting basic structure setup.

--- lib/efc-match-reports-stack.ts
+++ lib/efc-match-reports-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 
 export class EfcMatchReportsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const aggregatorLambda = new NodejsFunction(this, "aggregatorLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      entry: "./src/lambda/MatchAggregatorLambda.ts",
+      handler: "handler",
+      environment: {},
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EfcMatchReportsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
