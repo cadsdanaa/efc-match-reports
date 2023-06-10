1a799b50b067c9d5565a6a703240ff70694cd84f
Alex Dana <cadsdanaa@gmail.com>
2023-06-10 17:24:45 -0500

    Pushing up some lambda code to test the performance.

--- lib/efc-match-reports-stack.ts
+++ lib/efc-match-reports-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {Runtime} from "aws-cdk-lib/aws-lambda";
+import {Duration} from "aws-cdk-lib";
 
 export class EfcMatchReportsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,8 @@ export class EfcMatchReportsStack extends cdk.Stack {
 
     const aggregatorLambda = new NodejsFunction(this, "aggregatorLambda", {
       runtime: Runtime.NODEJS_18_X,
+      memorySize: 128,
+      timeout: Duration.minutes(1),
       entry: "./src/lambda/MatchAggregatorLambda.ts",
       handler: "handler",
       environment: {},
--- src/lambda/MatchAggregatorLambda.ts
+++ src/lambda/MatchAggregatorLambda.ts
@@ -1,5 +1,11 @@
 import { Handler } from 'aws-lambda';
+import {MatchDataService} from "../match/matchDataService";
+import axios from "axios";
+import {buildMatchOverviewStats} from "../stats/matchOverview";
 
 export const handler: Handler = async (event) => {
-    console.log('EVENT: ' + JSON.stringify(event));
+    const matchDataService = new MatchDataService(axios);
+    const matchData = await matchDataService.retrieveMatchData('3901273');
+    const matchOverview = buildMatchOverviewStats(matchData, 8668);
+    console.log(JSON.stringify(matchOverview));
 };
