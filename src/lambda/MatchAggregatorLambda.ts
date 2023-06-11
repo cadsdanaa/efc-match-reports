bb3096cc6c5d0494fb5bb020af0c3dc97b0612ef
Alex Dana <cadsdanaa@gmail.com>
2023-06-11 01:17:24 -0500

    Performance of lambda was pretty decent. Looking to create an html doc to display the overview stats in a cool way which I will maybe then convert to an image and then post to the Everton subreddit? Not completely sure yet but giving it a shot.

--- src/lambda/MatchAggregatorLambda.ts
+++ src/lambda/MatchAggregatorLambda.ts
@@ -1,11 +1,5 @@
 import { Handler } from 'aws-lambda';
-import {MatchDataService} from "../match/matchDataService";
-import axios from "axios";
-import {buildMatchOverviewStats} from "../stats/matchOverview";
 
 export const handler: Handler = async (event) => {
-    const matchDataService = new MatchDataService(axios);
-    const matchData = await matchDataService.retrieveMatchData('3901273');
-    const matchOverview = buildMatchOverviewStats(matchData, 8668);
-    console.log(JSON.stringify(matchOverview));
+    console.log('Test');
 };
